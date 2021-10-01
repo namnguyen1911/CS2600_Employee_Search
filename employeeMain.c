6e19d2328ffc7bd9aaed2819aa3696ef181f4cf8
Nam Nguyen <namnguyen@cpp.edu>
2021-10-01 03:09:48 -0700

    Add test for searchEmployeeByPhone and searchEmployeeBySalary

--- employeeMain.c
+++ employeeMain.c
@@ -8,6 +8,8 @@ int main(void) {
     //defined in employeeSearchOne.c
     PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind);
     PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char * nameToFind);
+    PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary);
+    PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char* phone);
 
     //defined in employeeTable.c
     extern Employee EmployeeTable[];
@@ -29,6 +31,33 @@ int main(void) {
     else 
         printf("Employee Tony Bobcat is NOT found in the record\n");
 
+    //Example not found
+    matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "714-111-1111");
+    if(matchPtr != NULL)
+        printf("Employee phone 714-111-1111 is in record %d\n", matchPtr - EmployeeTable);
+    else 
+        printf("Employee phone is NOT found in the record\n");
+
+    //Example found
+    matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "714-555-2749");
+    if(matchPtr != NULL)
+        printf("Employee phone 714-555-2749 is in record %d\n", matchPtr - EmployeeTable);
+    else 
+        printf("Employee phone is NOT found in the record\n");
+
+    //Example not found
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 20.0);
+    if(matchPtr != NULL)
+        printf("Employee salary 20.0 is in record %d\n", matchPtr - EmployeeTable);
+    else 
+        printf("Employee salary is NOT found in the record\n");
+
+    //Example found
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 4.50);
+    if(matchPtr != NULL)
+        printf("Employee salary 4.50 is in record %d\n", matchPtr - EmployeeTable);
+    else 
+        printf("Employee salary is NOT found in the record\n");
     
     return EXIT_SUCCESS;
 }
